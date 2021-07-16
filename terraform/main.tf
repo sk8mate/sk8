6dc088468fed279bea5ba6ec500987a39829bb5c
sk8mate <sk8mate@protonmail.com>
2021-07-16 10:35:17 +0200

    store terraform state in s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,40 @@ provider "aws" {
     alias = "us_east_1"
     region = "us-east-1"
 }
+
+# Backend
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "sk8-terraform-state"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "sk8_terraform_locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "sk8-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "sk8_terraform_locks"
+    encrypt        = true
+  }
+}
