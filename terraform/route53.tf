bf4b3e14a3ccd6ade48fd91e48d9a0782f03e8bf
sk8mate <sk8mate@protonmail.com>
2021-07-16 02:17:05 +0200

    add cache ttl

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -94,6 +94,9 @@ resource "aws_cloudfront_distribution" "web_sk8_town" {
     }
     target_origin_id = local.web_sk8_town_origin_id
     viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
   price_class = "PriceClass_100"
