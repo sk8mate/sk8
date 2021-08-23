a620551eb2b2fc953ae670b5647aa930ac58efb0
sk8mate <sk8mate@protonmail.com>
2021-08-23 22:44:11 +0200

    limit get places response fields to only those we use

--- backside/places/domain/domain.go
+++ backside/places/domain/domain.go
@@ -10,85 +10,21 @@ type Position struct {
 }
 
 type Poi struct {
-	Name        string `json:"name"`
-	Phone       string `json:"phone"`
-	CategorySet []struct {
-		ID int `json:"id"`
-	} `json:"categorySet"`
-	URL             string   `json:"url"`
-	Categories      []string `json:"categories"`
-	Classifications []struct {
-		Code  string `json:"code"`
-		Names []struct {
-			NameLocale string `json:"nameLocale"`
-			Name       string `json:"name"`
-		} `json:"names"`
-	} `json:"classifications"`
+	Name string `json:"name"`
 }
 
 type Address struct {
-	Municipality                string `json:"municipality"`
-	CountrySecondarySubdivision string `json:"countrySecondarySubdivision"`
-	CountrySubdivision          string `json:"countrySubdivision"`
-	CountryCode                 string `json:"countryCode"`
-	Country                     string `json:"country"`
-	CountryCodeISO3             string `json:"countryCodeISO3"`
-	FreeformAddress             string `json:"freeformAddress"`
+	FreeformAddress string `json:"freeformAddress"`
 }
 
 type Result struct {
-	Type       string   `json:"type"`
-	ID         string   `json:"id"`
-	Score      float64  `json:"score"`
-	EntityType string   `json:"entityType,omitempty"`
-	Address    Address  `json:"address"`
-	Position   Position `json:"position"`
-	Viewport   struct {
-		TopLeftPoint struct {
-			Lat float64 `json:"lat"`
-			Lon float64 `json:"lon"`
-		} `json:"topLeftPoint"`
-		BtmRightPoint struct {
-			Lat float64 `json:"lat"`
-			Lon float64 `json:"lon"`
-		} `json:"btmRightPoint"`
-	} `json:"viewport"`
-	BoundingBox struct {
-		TopLeftPoint struct {
-			Lat float64 `json:"lat"`
-			Lon float64 `json:"lon"`
-		} `json:"topLeftPoint"`
-		BtmRightPoint struct {
-			Lat float64 `json:"lat"`
-			Lon float64 `json:"lon"`
-		} `json:"btmRightPoint"`
-	} `json:"boundingBox,omitempty"`
-	DataSources struct {
-		Geometry struct {
-			ID string `json:"id"`
-		} `json:"geometry"`
-	} `json:"dataSources,omitempty"`
-	Info        string `json:"info,omitempty"`
-	Poi         Poi    `json:"poi,omitempty"`
-	EntryPoints []struct {
-		Type     string `json:"type"`
-		Position struct {
-			Lat float64 `json:"lat"`
-			Lon float64 `json:"lon"`
-		} `json:"position"`
-	} `json:"entryPoints,omitempty"`
+	Type     string   `json:"type"`
+	Address  Address  `json:"address"`
+	Position Position `json:"position"`
+	Poi      Poi      `json:"poi,omitempty"`
 }
 
 type GetPlacesResponse struct {
-	Summary struct {
-		Query        string `json:"query"`
-		QueryType    string `json:"queryType"`
-		QueryTime    int    `json:"queryTime"`
-		NumResults   int    `json:"numResults"`
-		Offset       int    `json:"offset"`
-		TotalResults int    `json:"totalResults"`
-		FuzzyLevel   int    `json:"fuzzyLevel"`
-	} `json:"summary"`
 	Results []Result `json:"results"`
 }
 
