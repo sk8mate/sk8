179854c51d7f23d34966a93c8e7eac7af698e174
michalovsky <michalovskyyy@gmail.com>
2021-09-12 16:36:38 +0200

    add domain struct

--- backside/spots/domain/domain.go
+++ backside/spots/domain/domain.go
@@ -1,13 +1,15 @@
 package domain
 
 type Spot struct {
-	Name string `db:"name"`
-	Address string `db:"address"`
-	Coordinates struct {
-		Lat  float64
-		Long float64
-	} `db:"coordinates"`
-	Lighting bool `db:"lighting"`
-	Friendly bool `db:"friendly"`
-	Verified bool `db:"verified"`
+	Name        string      `db:"name"`
+	Address     string      `db:"address"`
+	Coordinates Coordinates `db:"coordinates"`
+	Lighting    bool        `db:"lighting"`
+	Friendly    bool        `db:"friendly"`
+	Verified    bool        `db:"verified"`
+}
+
+type Coordinates struct {
+	Lat  float64 `db:"lat"`
+	Long float64 `db:"long"`
 }
