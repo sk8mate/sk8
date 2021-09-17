9ecb4d8ddbf45ad936e2097088da9f414967f90d
sk8mate <sk8mate@protonmail.com>
2021-09-17 02:03:58 +0200

    incomplete showSearch

--- frontside/lib/views/home/home.dart
+++ frontside/lib/views/home/home.dart
@@ -1,5 +1,48 @@
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 import 'package:frontside/parts/navbar.dart';
+import 'package:frontside/views/home/autocomplete_response.pb.dart';
+import 'package:frontside/views/home/autocomplete_request.pb.dart';
+
+class PlacesSearchDelegate extends SearchDelegate<String> {
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, '');
+      },
+    );
+  }
+
+  Future<List<AutocompleteEntryResponse>> _getSuggestions() {}
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+      builder: (BuildContext context,
+          AsyncSnapshot<List<AutocompleteEntryResponse>> response) {
+        if (!response.hasData) {
+          return Text('No results');
+        }
+        return ListView.builder(
+          itemBuilder: (context, index) => ListTile(
+            title: Text(response.data![index]),
+            onTap: () {
+              close(context, response.data![index]);
+            },
+          ),
+          itemCount: suggestions.data!.length,
+        );
+      },
+      future: _getSuggestions(),
+    );
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) => <Widget>[];
+}
 
 class Home extends StatelessWidget {
   static const String route = "/";
@@ -9,17 +52,21 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        centerTitle: true,
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        child: Image(
-          image: AssetImage('assets/example_screens/mapsicle-map.png'),
-          fit: BoxFit.none,
+      body: Column(children: [
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: TextField(
+            readOnly: true,
+            onTap: () {
+              showSearch(context: context, delegate: PlacesSearchDelegate());
+            },
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(16))),
+          ),
         ),
-      ),
+      ]),
       bottomNavigationBar: NavBar(),
     );
   }
