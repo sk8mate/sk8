dd291baf0aa6f4bcbb3a39ae9e83f221518d4852
michalovsky <michalovskyyy@gmail.com>
2021-09-26 11:49:39 +0200

    extract write response function into separate package

--- backside/spots/handler.go
+++ backside/spots/handler.go
@@ -6,6 +6,7 @@ import (
 	"sk8.town/backside/errs"
 
 	"sk8.town/backside/spots/dto"
+	"sk8.town/backside/utils"
 )
 
 type Handler struct {
@@ -16,23 +17,14 @@ func (handler Handler) AddSpot(writer http.ResponseWriter, request *http.Request
 	var spotRequest dto.SpotRequest
 	if err := json.NewDecoder(request.Body).Decode(&spotRequest); err != nil {
 		appError := errs.NewBadRequestError("")
-		writeResponse(writer, appError.Code, appError.AsMessage())
+		utils.WriteResponse(writer, appError.Code, appError.AsMessage())
 		return
 	}
 
 	response, appError := handler.service.Add(&spotRequest)
 	if appError != nil {
-		writeResponse(writer, appError.Code, appError.AsMessage())
+		utils.WriteResponse(writer, appError.Code, appError.AsMessage())
 	} else {
-		writeResponse(writer, http.StatusOK, response)
-	}
-}
-
-func writeResponse(writer http.ResponseWriter, code int, data interface{}) {
-	writer.Header().Add("Content-Type", "application/json")
-	writer.WriteHeader(code)
-	err := json.NewEncoder(writer).Encode(data)
-	if err != nil {
-		panic(err)
+		utils.WriteResponse(writer, http.StatusOK, response)
 	}
 }
---
+++ backside/utils/http_utils.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func WriteResponse(writer http.ResponseWriter, code int, data interface{}) {
+	writer.Header().Add("Content-Type", "application/json")
+	writer.WriteHeader(code)
+	err := json.NewEncoder(writer).Encode(data)
+	if err != nil {
+		panic(err)
+	}
+}
