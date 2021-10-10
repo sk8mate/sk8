512f864ee19b8276f47206ec44b75283e5cf3d12
michalovsky <michalovskyyy@gmail.com>
2021-10-10 19:08:08 +0200

    add update spots method tests in handler tests

--- backside/spots/handler_test.go
+++ backside/spots/handler_test.go
@@ -108,7 +108,7 @@ func Test_get_spot_given_correct_request_should_return_spot_with_status_200(t *t
 	teardown := setup(t)
 	defer teardown()
 	spotGetData := dto.SpotsGetData{
-		Id: "5",
+		Id:      "5",
 		Name:    "Dom",
 		Address: "Grzegorzecka 79f Krakow",
 		Coordinates: &dto.ResponseCoordinates{
@@ -193,6 +193,73 @@ func Test_get_spots_given_service_error_should_return_service_error(t *testing.T
 	serviceMock.EXPECT().GetAll().Return(nil, errs.NewNotFoundError(""))
 	router.HandleFunc("/spots", handler.GetSpots)
 	request, _ := http.NewRequest(http.MethodPost, "/spots", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	assert.Equal(t, http.StatusNotFound, recorder.Code)
+	expectedResponse := `{"status":"error","message":"Not found"}`
+	assert.Equal(t, expectedResponse, strings.TrimSpace(recorder.Body.String()))
+}
+
+func Test_update_spot_given_correct_request_should_return_updated_spot_with_status_200(t *testing.T) {
+	teardown := setup(t)
+	defer teardown()
+	updateSpotRequest := dto.SpotsUpdateRequest{
+		Name: "Pizza",
+	}
+	updatedSpotData := dto.SpotsUpdateData{
+		Id:      "5",
+		Name:    "Pizza",
+		Address: "Pawia 5",
+		Coordinates: &dto.ResponseCoordinates{
+			Lat:  40,
+			Long: 60,
+		},
+		Lighting: false,
+		Friendly: false,
+		Verified: false,
+	}
+	id := 5
+	serviceMock.EXPECT().Update(id, &updateSpotRequest).Return(&updatedSpotData, nil)
+	router.HandleFunc("/spots/{id:[0-9]+}", handler.UpdateSpot)
+	var jsonStr = []byte(`{"name":"Pizza"}`)
+	request, _ := http.NewRequest(http.MethodPut, "/spots/5", bytes.NewBuffer(jsonStr))
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	expectedResponse := `{"status":"success","data":{"id":"5","name":"Pizza","address":"Pawia 5","coordinates":{"lat":40,"long":60}}}`
+	assert.Equal(t, expectedResponse, strings.TrimSpace(recorder.Body.String()))
+}
+
+func Test_update_spot_given_bad_request_should_return_400(t *testing.T) {
+	teardown := setup(t)
+	defer teardown()
+	router.HandleFunc("/spots/{id:[0-9]+}", handler.UpdateSpot)
+	var jsonStr = []byte(``)
+	request, _ := http.NewRequest(http.MethodPut, "/spots/5", bytes.NewBuffer(jsonStr))
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	assert.Equal(t, http.StatusBadRequest, recorder.Code)
+	expectedResponse := `{"status":"error","message":"Bad request"}`
+	assert.Equal(t, expectedResponse, strings.TrimSpace(recorder.Body.String()))
+}
+
+func Test_update_spot_given_service_error_should_return_service_error(t *testing.T) {
+	teardown := setup(t)
+	defer teardown()
+	updateSpotRequest := dto.SpotsUpdateRequest{
+		Name: "Pizza",
+	}
+	id := 5
+	serviceMock.EXPECT().Update(id, &updateSpotRequest).Return(nil, errs.NewNotFoundError(""))
+	router.HandleFunc("/spots/{id:[0-9]+}", handler.UpdateSpot)
+	var jsonStr = []byte(`{"name":"Pizza"}`)
+	request, _ := http.NewRequest(http.MethodPut, "/spots/5", bytes.NewBuffer(jsonStr))
 	recorder := httptest.NewRecorder()
 
 	router.ServeHTTP(recorder, request)
