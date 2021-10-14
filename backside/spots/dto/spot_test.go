439252108ca662b6f8f0273272aeb4acba0b4957
michalovsky <michalovskyyy@gmail.com>
2021-10-14 21:22:26 +0200

    add spot tests from proto validation

--- backside/spots/dto/spot_test.go
+++ backside/spots/dto/spot_test.go
@@ -1,15 +1,43 @@
 package dto
 
 import (
-	"net/http"
-	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_given_spot_without_name_should_return_error(t *testing.T) {
+	request := &SpotsAddRequest{
+		Address:     "Pawia 5",
+		Coordinates: nil,
+		Lighting:    true,
+		Friendly:    true,
+		Verified:    true,
+	}
+
+	err := request.Validate()
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid SpotsAddRequest.Name: value length must be at least 1 runes", err.Error())
+}
+
+func Test_given_spot_without_address_should_return_error(t *testing.T) {
+	request := &SpotsAddRequest{
+		Name:        "Dworzec Glowny Krakow",
+		Coordinates: nil,
+		Lighting:    true,
+		Friendly:    true,
+		Verified:    true,
+	}
+
+	err := request.Validate()
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid SpotsAddRequest.Address: value length must be at least 1 runes", err.Error())
+}
+
 func Test_given_spot_without_coordinates_should_return_error(t *testing.T) {
-	request := SpotsAddRequest{
+	request := &SpotsAddRequest{
 		Name:        "Dworzec Glowny Krakow",
 		Address:     "Pawia 5",
 		Coordinates: nil,
@@ -18,17 +46,17 @@ func Test_given_spot_without_coordinates_should_return_error(t *testing.T) {
 		Verified:    true,
 	}
 
-	appError := request.Validate()
+	err := request.Validate()
 
-	assert.Equal(t, http.StatusUnprocessableEntity, appError.Code)
-	assert.Regexp(t, regexp.MustCompile(`Coordinates not provided`), appError.Message)
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid SpotsAddRequest.Coordinates: value is required", err.Error())
 }
 
 func Test_given_valid_spot_should_return_success(t *testing.T) {
 	request := SpotsAddRequest{
 		Name:    "Dworzec Glowny Krakow",
 		Address: "Pawia 5",
-		Coordinates: &RequestCoordinates{
+		Coordinates: &SpotsAddRequest_Coordinates{
 			Lat:  40,
 			Long: 60,
 		},
