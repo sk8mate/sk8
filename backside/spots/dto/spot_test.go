b979ab87b2f409a0cb4d5c6a9025f571069e4ab9
michalovsky <michalovskyyy@gmail.com>
2021-10-15 15:59:04 +0200

    add update spot requests validation tests

--- backside/spots/dto/spot_test.go
+++ backside/spots/dto/spot_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_given_spot_without_name_should_return_error(t *testing.T) {
+func Test_given_add_spot_request_without_name_should_return_error(t *testing.T) {
 	request := &SpotsAddRequest{
 		Address:     "Pawia 5",
 		Coordinates: nil,
@@ -21,7 +21,7 @@ func Test_given_spot_without_name_should_return_error(t *testing.T) {
 	assert.Equal(t, "invalid SpotsAddRequest.Name: value length must be at least 1 runes", err.Error())
 }
 
-func Test_given_spot_without_address_should_return_error(t *testing.T) {
+func Test_given_add_spot_request_without_address_should_return_error(t *testing.T) {
 	request := &SpotsAddRequest{
 		Name:        "Dworzec Glowny Krakow",
 		Coordinates: nil,
@@ -36,7 +36,7 @@ func Test_given_spot_without_address_should_return_error(t *testing.T) {
 	assert.Equal(t, "invalid SpotsAddRequest.Address: value length must be at least 1 runes", err.Error())
 }
 
-func Test_given_spot_without_coordinates_should_return_error(t *testing.T) {
+func Test_given_add_spot_request_without_coordinates_should_return_error(t *testing.T) {
 	request := &SpotsAddRequest{
 		Name:        "Dworzec Glowny Krakow",
 		Address:     "Pawia 5",
@@ -52,11 +52,48 @@ func Test_given_spot_without_coordinates_should_return_error(t *testing.T) {
 	assert.Equal(t, "invalid SpotsAddRequest.Coordinates: value is required", err.Error())
 }
 
-func Test_given_valid_spot_should_return_success(t *testing.T) {
+func Test_given_valid_add_spot_request_should_return_success(t *testing.T) {
 	request := SpotsAddRequest{
 		Name:    "Dworzec Glowny Krakow",
 		Address: "Pawia 5",
 		Coordinates: &SpotsAddRequest_Coordinates{
+			Lat:  40,
+			Long: 60,
+		},
+		Lighting: true,
+		Friendly: true,
+		Verified: true,
+	}
+
+	err := request.Validate()
+
+	assert.Nil(t, err)
+}
+
+func Test_given_update_spot_request_with_invalid_coordinates_should_return_error(t *testing.T) {
+	request := &SpotsUpdateRequest{
+		Name:    "Dworzec Glowny Krakow",
+		Address: "Pawia 5",
+		Coordinates: &SpotsUpdateRequest_Coordinates{
+			Lat:  700,
+			Long: 100,
+		},
+		Lighting: true,
+		Friendly: true,
+		Verified: true,
+	}
+
+	err := request.Validate()
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "invalid SpotsUpdateRequest.Coordinates: embedded message failed validation | caused by: invalid SpotsUpdateRequest_Coordinates.Lat: value must be inside range [-90, 90]", err.Error())
+}
+
+func Test_given_valid_update_spot_request_should_return_success(t *testing.T) {
+	request := SpotsUpdateRequest{
+		Name:    "Dworzec Glowny Krakow",
+		Address: "Pawia 5",
+		Coordinates: &SpotsUpdateRequest_Coordinates{
 			Lat:  40,
 			Long: 60,
 		},
