1ee14ebfc8d484e6b85a99773037a75b2f2796be
michalovsky <michalovskyyy@gmail.com>
2021-10-18 18:39:56 +0200

    add function for writing proto responses

--- backside/utils/http_utils.go
+++ backside/utils/http_utils.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"encoding/json"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
 	"net/http"
 
 	"sk8.town/backside/errs"
@@ -32,6 +34,16 @@ func WriteJSON(writer http.ResponseWriter, code int, data interface{}) {
 	writer.Header().Add("Content-Type", "application/json")
 	writer.WriteHeader(code)
 	err := json.NewEncoder(writer).Encode(data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func WriteProtoMessage(writer http.ResponseWriter, code int, data proto.Message) {
+	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+	writer.WriteHeader(code)
+	m := jsonpb.Marshaler{EmitDefaults: true}
+	err := m.Marshal(writer, data)
 	if err != nil {
 		panic(err)
 	}
