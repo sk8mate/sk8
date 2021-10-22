71adf669c48b13a1adf3d4226c0d9c6f4bc1c35e
michalovsky <michalovskyyy@gmail.com>
2021-10-23 00:32:49 +0200

    add filter to dto functions

--- backside/spots/domain/filter.go
+++ backside/spots/domain/filter.go
@@ -1,5 +1,10 @@
 package domain
 
+import (
+	"sk8.town/backside/spots/dto"
+	"strconv"
+)
+
 type Filter struct {
 	ID           int `gorm:"primary_key;AUTO_INCREMENT"`
 	Name         string
@@ -11,4 +16,26 @@ type FilterValue struct {
 	ID       int `gorm:"primary_key;AUTO_INCREMENT"`
 	Value    string
 	FilterID int
+}
+
+func (f Filter) ToDto() *dto.FilterData {
+	var dtoFilterValues []*dto.FilterValueData
+
+	for _, filterValue := range f.FilterValues {
+		dtoFilterValues = append(dtoFilterValues, filterValue.ToDto())
+	}
+
+	return &dto.FilterData{
+		Id:     strconv.Itoa(f.ID),
+		Name:   f.Name,
+		Type:   f.Type,
+		Values: dtoFilterValues,
+	}
+}
+
+func (fv FilterValue) ToDto() *dto.FilterValueData {
+	return &dto.FilterValueData{
+		Id:   strconv.Itoa(fv.ID),
+		Name: fv.Value,
+	}
 }
