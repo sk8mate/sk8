159ea1f34c9f24e7514e6d9c51ac988311f1e59b
michalovsky <michalovskyyy@gmail.com>
2021-11-24 18:05:16 +0100

    add google claims

--- backside/auth/google_claims.go
+++ backside/auth/google_claims.go
@@ -1,11 +1,9 @@
 package auth
 
-import "github.com/dgrijalva/jwt-go"
-
 type GoogleClaims struct {
-	Email         string `json:"email"`
-	EmailVerified bool   `json:"email_verified"`
-	FirstName     string `json:"given_name"`
-	LastName      string `json:"family_name"`
-	jwt.StandardClaims
+	Id            string
+	Email         string
+	EmailVerified bool
+	FirstName     string
+	LastName      string
 }
