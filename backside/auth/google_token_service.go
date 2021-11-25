96de2c7c48b4ea2068da2753fefc1e3deb1474b8
michalovsky <michalovskyyy@gmail.com>
2021-11-25 14:47:14 +0100

    add additional payload validation

--- backside/auth/google_token_service.go
+++ backside/auth/google_token_service.go
@@ -5,9 +5,50 @@ import (
 	"google.golang.org/api/idtoken"
 	"sk8.town/backside/auth/config"
 	"sk8.town/backside/errs"
+	"time"
 )
 
 type GoogleTokenService struct {
+}
+
+func (s GoogleTokenService) verifyPayload(payload *idtoken.Payload) (*UserClaims, *errs.AppError) {
+	if payload.Issuer != "accounts.google.com" && payload.Issuer != "https://accounts.google.com" {
+		return nil, errs.NewUnexpectedError("iss is invalid")
+	}
+
+	if payload.Expires < time.Now().UTC().Unix() {
+		return nil, errs.NewUnexpectedError("token is expired")
+	}
+
+	email, ok := payload.Claims["email"].(string)
+	if !ok {
+		return nil, errs.NewUnexpectedError("email field in payload is invalid")
+	}
+
+	emailVerified, ok := payload.Claims["email_verified"].(bool)
+	if !ok {
+		return nil, errs.NewUnexpectedError("email_verified field in payload is invalid")
+	}
+
+	firstName, ok := payload.Claims["given_name"].(string)
+	if !ok {
+		return nil, errs.NewUnexpectedError("given_name field in payload is invalid")
+	}
+
+	lastName, ok := payload.Claims["family_name"].(string)
+	if !ok {
+		return nil, errs.NewUnexpectedError("family_name field in payload is invalid")
+	}
+
+	userClaims := UserClaims{
+		Id:            payload.Subject,
+		Email:         email,
+		EmailVerified: emailVerified,
+		FirstName:     firstName,
+		LastName:      lastName,
+	}
+
+	return &userClaims, nil
 }
 
 func (s GoogleTokenService) Verify(token string) (*UserClaims, *errs.AppError) {
@@ -17,18 +58,14 @@ func (s GoogleTokenService) Verify(token string) (*UserClaims, *errs.AppError) {
 		return nil, errs.NewUnexpectedError(err.Error())
 	}
 
-	googleClaims:= UserClaims{
-		Id:            payload.Subject,
-		Email:         payload.Claims["email"].(string),
-		EmailVerified: payload.Claims["email_verified"].(bool),
-		FirstName:     payload.Claims["given_name"].(string),
-		LastName:      payload.Claims["family_name"].(string),
+	googleUserClaims, appError := s.verifyPayload(payload)
+	if appError != nil{
+		return nil, appError
 	}
 
-	return &googleClaims, nil
+	return googleUserClaims, nil
 }
 
 func NewGoogleTokenService() GoogleTokenService {
 	return GoogleTokenService{}
 }
-
