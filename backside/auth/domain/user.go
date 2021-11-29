4020506db86f741e1fd28d062116b536aa7eca89
michalovsky <michalovskyyy@gmail.com>
2021-11-29 15:41:52 +0100

    add bson tags to user struct

--- backside/auth/domain/user.go
+++ backside/auth/domain/user.go
@@ -1,9 +1,17 @@
 package domain
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
 type User struct {
-	Id        string
-	FirstName string
-	LastName  string
-	Email     string
-	Password  string
+	Id        primitive.ObjectID `bson:"_id"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
+	FirstName string             `bson:"first_name"`
+	LastName  string             `bson:"last_name"`
+	Email     string             `bson:"email"`
+	Password  string             `bson:"password"`
+	OAuthId   string             `bson:"oauth_id"`
 }
